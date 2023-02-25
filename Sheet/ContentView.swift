6bcbc0a2faf957e4d52c5e6c4e3a1f6d20b44228
Dalton Turner <turnerdalton055@gmail.com>
2023-02-24 16:45:21 -0800

    Adds TCA as a dep. and creates basic views

--- Sheet/ContentView.swift
+++ Sheet/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "cooktop")
-                .imageScale(.large)
-                .foregroundColor(.blue)
-            Text("Sheet")
+        NavigationStack {
+            List {
+                Section {
+                    NavigationLink {
+                        RecipeDetailView(recipeName: "Gnocci")
+                    } label: {
+                        Text("Gnocci")
+                    }
+                    NavigationLink {
+                        RecipeDetailView(recipeName: "Cacio e Pepe")
+                    } label: {
+                        Text("Cacio e Pepe")
+                    }
+                    NavigationLink {
+                        RecipeDetailView(recipeName: "Spagetti")
+                    } label: {
+                        Text("Spagetti")
+                    }
+                } header: {
+                    Text("Pasta")
+                }
+                Section {
+                    NavigationLink {
+                        RecipeDetailView(recipeName: "Cesar")
+                    } label: {
+                        Text("Cesar")
+                    }
+                    NavigationLink {
+                        RecipeDetailView(recipeName: "Strawberry Spinach")
+                    } label: {
+                        Text("Strawberry Spinach")
+                    }
+                } header: {
+                    Text("Salads")
+                }
+            }
+            .navigationTitle(Text("Recipes"))
         }
-        .padding()
     }
 }
 
