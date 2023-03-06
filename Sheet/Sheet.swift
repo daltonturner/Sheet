37a90bf1df70740595a97e0fc2e3951b94d0b5b5
Dalton Turner <turnerdalton055@gmail.com>
2023-03-05 19:01:01 -0800

    Adding business logic for Sheet

---
+++ Sheet/Sheet.swift
@@ -0,0 +1,40 @@
+//
+//  Sheet.swift
+//  Sheet
+//
+//  Created by Dalton Turner on 3/5/23.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct Sheet: ReducerProtocol {
+    public struct State: Equatable {
+        var filterQuery: String
+        var recipes: [Recipe]
+
+        static let initial = State(filterQuery: "", recipes: [])
+    }
+
+    public enum Action {
+        case recipeWasSelected(name: String)
+        case recipesLoaded([Recipe])
+        case filterQueryChanged(String)
+        case loadRecipes
+    }
+
+    public func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .recipeWasSelected:
+            return .none
+        case .recipesLoaded(let recipes):
+            state.recipes = recipes
+            return .none
+        case .filterQueryChanged(let query):
+            state.filterQuery = query
+            return .none
+        case .loadRecipes:
+            return .send(.recipesLoaded([]))
+        }
+    }
+}
